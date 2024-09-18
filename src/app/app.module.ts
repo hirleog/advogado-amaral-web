5e4a1de0c4fc9bf8bdef7727e863fa4b3f1dfb7d
Hirle <87393381+hirleog@users.noreply.github.com>
2024-09-18 13:33:15 -0300

    new banner layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,24 @@
+import { FooterComponent } from './template/footer/footer.component';
+import { NavComponent } from './template/nav/nav.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
+import { WhatPeopleSayComponent } from './pages/what-people-say/what-people-say.component';
+import { ScreenServiceComponent } from './pages/screen-service/screen-service.component';
+import { SliderComponent } from './pages/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    SliderComponent,
+    ScreenServiceComponent,
+    WhatPeopleSayComponent,
+    AboutComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
